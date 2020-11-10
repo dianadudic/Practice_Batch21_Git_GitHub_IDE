2b67576854d0b7b55ca03d6da1379b3299839b42
dianadudic <dianadudic@gmail.com>
2020-11-10 14:47:47 -0500

    did i ignore it

--- src/practice2_day2.java
+++ src/practice2_day2.java
@@ -2,5 +2,7 @@ public class practice2_day2 {
     public static void main(String[] args) {
         System.out.println("what is next?");
         //why are we ignoring all of this
+        //rollback
+
     }
 }
