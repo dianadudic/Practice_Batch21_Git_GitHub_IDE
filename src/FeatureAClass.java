ce2302e902e38bdf02a70195a257ca2cc23a60e6
dianadudic <dianadudic@gmail.com>
2020-11-10 15:42:34 -0500

    adding more lines

--- src/FeatureAClass.java
+++ src/FeatureAClass.java
@@ -1,5 +1,7 @@
 public class FeatureAClass {
     public static void main(String[] args) {
         System.out.println("difference between master and feature a");
+        //some more lines
+        //so we can commit and see where the head is
     }
 }
